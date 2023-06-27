b22392148e7342a9e4107e4c5677924285058af2
bagoviggo <bagoviggo@gmail.com>
2023-06-27 19:24:20 +0300

    Add accounts app

--- kejani_io/settings.py
+++ kejani_io/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'contacts.apps.ContactsConfig', #new
-
+    'accounts.apps.AccountsConfig', # new 
+   
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
