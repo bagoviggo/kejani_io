736821f9c74f4c0e3a25440c262a3ceafe65d4b6
bagoviggo <bagoviggo@gmail.com>
2023-07-08 00:37:06 +0300

    Add TestCase for Contacts

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,1 +1,76 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+class AccountsTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.register_url = reverse('register')
+        self.login_url = reverse('login')
+        self.dashboard_url = reverse('dashboard')
+        self.username = 'testuser'
+        self.password = 'testpassword'
+        self.user = User.objects.create_user(username=self.username, password=self.password)
+
+    def test_register_valid_form(self):
+        response = self.client.post(self.register_url, {
+            'username': 'newuser',
+            'password': 'newpassword',
+            'password2': 'newpassword',
+            'email': 'newuser@example.com',
+            'first_name': 'New',
+            'last_name': 'User',
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('login'))
+        self.assertEqual(User.objects.filter(username='newuser').count(), 1)
+
+    def test_register_invalid_form(self):
+        response = self.client.post(self.register_url, {
+            'username': 'newuser',
+            'password': 'newpassword',
+            'password2': 'differentpassword',
+            'email': 'newuser@example.com',
+            'first_name': 'New',
+            'last_name': 'User',
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'accounts/register.html')
+        self.assertEqual(User.objects.filter(username='newuser').count(), 0)
+
+    def test_login_valid_credentials(self):
+        response = self.client.post(self.login_url, {
+            'username': self.username,
+            'password': self.password,
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('dashboard'))
+
+    def test_login_invalid_credentials(self):
+        response = self.client.post(self.login_url, {
+            'username': 'invaliduser',
+            'password': 'invalidpassword',
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'accounts/login.html')
+
+    def test_logout(self):
+        self.client.login(username=self.username, password=self.password)
+        response = self.client.post(reverse('logout'))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('index'))
+        # Check if user is logged out
+        self.assertNotIn('_auth_user_id', self.client.session)
+
+    def test_dashboard_authenticated_user(self):
+        self.client.login(username=self.username, password=self.password)
+        response = self.client.get(self.dashboard_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'accounts/dashboard.html')
+        # Add additional assertions based on the expected behavior
+
+    def test_dashboard_unauthenticated_user(self):
+        response = self.client.get(self.dashboard_url)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('login'))
 
